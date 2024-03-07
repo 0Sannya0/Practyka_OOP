087b12c2dc6c47ddbb3f8b9c3e9f19a061e6436d
0Sannya0 <121272072+0Sannya0@users.noreply.github.com>
2024-03-07 21:39:48 +0200

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,35 +1,26 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         List<Integer> results = new ArrayList<>();
-        
-
         while (true) {
             System.out.println("1. Calculate max sequence length");
             System.out.println("2. Display calculation history");
             System.out.println("3. Exit");
             System.out.println("Enter your choice:");
-
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
                     System.out.println("Enter the value of alpha: ");
                     double alpha = scanner.nextDouble();
-
                     ParametersResult parametersResult = new ParametersResult();
                     parametersResult.setAlpha(alpha);
                     parametersResult.setMaxSequenceLength(10);
-
                     SolutionFinder solutionFinder = new SolutionFinder(parametersResult);
                     int maxSequenceLength = solutionFinder.calculateMaxSequenceLength();
-
                     results.add(maxSequenceLength);
-
                     System.out.println("Max sequence length: " + maxSequenceLength);
                     break;
                 case 2:
