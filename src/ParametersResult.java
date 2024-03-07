b6bc7b74321240493297433e65ff0b1bd7ca5dfb
0Sannya0 <121272072+0Sannya0@users.noreply.github.com>
2024-03-07 21:28:49 +0200

    Update ParametersResult.java

--- src/ParametersResult.java
+++ src/ParametersResult.java
@@ -1,19 +1,23 @@
-import java.io.Serializable;
-public class ParametersResult implements Serializable {
-    private static final long serialVersionUID = 1L;
+class ParametersResult {
     private double alpha;
     private int maxSequenceLength;
+
     public double getAlpha() {
         return alpha;
     }
+
     public void setAlpha(double alpha) {
-        this.alpha = alpha;}
+        this.alpha = alpha;
+    }
+
     public int getMaxSequenceLength() {
         return maxSequenceLength;
     }
+
     public void setMaxSequenceLength(int maxSequenceLength) {
         this.maxSequenceLength = maxSequenceLength;
     }
+
     @Override
     public String toString() {
         return "ParametersResult{" +
